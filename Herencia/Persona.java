3b8e76ca0220b32ca4f85b9335681e6e49527fe2
José Pablo GR <89881634+JosePablo-GR@users.noreply.github.com>
2024-12-13 14:22:18 -0600

    Create Persona.java

---
+++ Herencia/Persona.java
@@ -0,0 +1,50 @@
+public class Persona {
+
+	String nombre;
+	int edad;
+
+	public Persona (String name, int age) {
+
+		nombre = name;
+		edad = age;
+
+	}
+
+	public void saludar () {
+
+		System.out.println("Hola me llamo " + this.getNombre());
+
+	}
+
+	public void setNombre (String nombre) {
+
+		this.nombre = nombre;
+
+	}
+
+	public void setEdad (int edad) {
+
+		this.edad = edad;
+
+	}
+
+	public String getNombre () {
+
+		return nombre;
+
+	}
+
+	public int getEdad () {
+
+		return edad;
+
+	}
+
+	@Override
+	public String toString () {
+
+		return "Nombre: " + nombre + " de " + edad + " años.";
+
+	}
+
+}
